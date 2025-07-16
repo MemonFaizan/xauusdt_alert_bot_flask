845cd47dd5969a87478b4cea517836d53f112ef0
Memon Faizan <123394650+MemonFaizan@users.noreply.github.com>
2025-07-17 03:01:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import threading
-import bot  # your main signal script file
+import bot
 
 app = Flask(__name__)
 
@@ -8,11 +8,9 @@ app = Flask(__name__)
 def home():
     return "✅ Gold Sniper Bot Running!"
 
-# Directly start thread without using decorator
 def run_bot():
     bot.main()
 
-# Start thread before app.run()
 if __name__ == "__main__":
-    threading.Thread(target=run_bot).start()
+    threading.Thread(target=run_bot, daemon=True).start()
     app.run(host="0.0.0.0", port=10000)
